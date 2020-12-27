16c2155b2ebdc63c2ad2f12cca8d24c9d0eb9bc0
weboo <weboo@users.noreply.github.com>
2020-12-27 23:23:51 +0900

    added keymap for windows and support Oyayubi-Shift input

--- keyboards/ergodash/rev1/keymaps/weboo/keymap.c
+++ keyboards/ergodash/rev1/keymaps/weboo/keymap.c
@@ -1,18 +1,23 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "nicola.h"
+NGKEYS nicola_keys;
 
 extern keymap_config_t keymap_config;
 
-#define _QWERTY_WIN 0
-#define _QWERTY_MAC 1
-#define _LOWER 2
-#define _RAISE 3
-#define _ADJUST 16
+enum keymap_layers {
+  _QWERTY_WIN,
+  _QWERTY_MAC,
+  _NICOLA,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+};
 
 static uint16_t key_timer;
 
 enum custom_keycodes {
-  QWERTY_WIN = SAFE_RANGE,
+  QWERTY_WIN = NG_SAFE_RANGE,
   QWERTY_MAC,
   LOWER,
   RAISE,
@@ -20,6 +25,8 @@ enum custom_keycodes {
   VRSN,
   G_CX,
   C_CX,
+  KANA,
+  EISU,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -32,17 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |   [  |                    |   ]  |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Next |      |  Fn  |      |   N  |   M  |   ,  |   .  |   /  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |      | Next |      | C_CX |      |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+      +------+------+------+      +------+------+------+------+-------------|
-   * | C_CX | Paste|  Win | LAlt |||||||| Lower| Space| Win+S|||||||| Enter|  F14 | Raise|||||||| RAlt |  BS  | Home | End  |
+   * | PgUp | PgDn |  Win | EISU |||||||| Lower| Space| Win+S|||||||| Enter|  Meh | Raise|||||||| KANA |  BS  | Home | End  |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY_WIN] = LAYOUT( \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                          KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                         KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                         KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              LCTL(KC_TAB), KC_HELP,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    C_CX, LCTL(KC_V), KC_LGUI, ALT_T(KC_F15),        LOWER, KC_SPC, LGUI(KC_S),   KC_ENTER, KC_F14, RAISE,      ALT_T(KC_F14), KC_BSPC, KC_HOME, KC_END   \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            LCTL(KC_TAB), C_CX,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_PGUP,KC_PGDOWN,KC_LGUI, EISU,            LOWER,    KC_SPC, LGUI(KC_S),   KC_ENTER,  KC_MEH, RAISE,             KANA,    KC_BSPC, KC_HOME, KC_END   \
   ),
 
   /* Qwerty for Mac
@@ -62,8 +69,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                          KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                         KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                         KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              LCTL(KC_TAB), KC_HELP,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    G_CX, LGUI(KC_V), ALT_T(KC_F1), KC_LGUI,         LOWER, KC_SPC, LGUI(KC_SPC), KC_ENTER, KC_LANG1, RAISE,          KC_RGUI, KC_BSPC, KC_HOME, KC_END   \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            LCTL(KC_TAB), KC_HELP,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    G_CX, LGUI(KC_V), ALT_T(KC_F1), KC_LGUI,     LOWER,   KC_SPC, LGUI(KC_SPC), KC_ENTER, KC_LANG1, RAISE,            KC_RGUI, KC_BSPC, KC_HOME, KC_END   \
+  ),
+
+  /* NICOLA
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * |      |   .  |  ka  |  ta  |  ko  |  sa  |      |                    |      |  ra  |  ti  |  ku  |  tu  |   ,  |      |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * |      |   u  |  si  |  te  |  ke  |  se  |      |                    |      |  ha  |  to  |  ki  |   i  |  nn  |      |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * |      |   .  |  hi  |  su  |  hu  |  he  |      |      |      |      |      |  me  |  so  |  ne  |  ho  |   /  |      |
+   * |-------------+------+------+------+------+      +------+------+------+      +------+------+------+------+-------------|
+   * |      |      |      |      ||||||||      | OyaL |      ||||||||      | OyaR |      ||||||||      |      |      |      |
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+  [_NICOLA] = LAYOUT( \
+    _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, \
+    _______, NG_Q,    NG_W,    NG_E,    NG_R,    NG_T,    _______,                   _______, NG_Y,    NG_U,    NG_I,    NG_O,    NG_P,    _______, \
+    _______, NG_A,    NG_S,    NG_D,    NG_F,    NG_G,    _______,                   _______, NG_H,    NG_J,    NG_K,    NG_L,    NG_SCLN, _______, \
+    _______, NG_Z,    NG_X,    NG_C,    NG_V,    NG_B,              _______,_______,          NG_N,    NG_M,    NG_COMM, NG_DOT,  NG_SLSH, _______, \
+    _______, _______, _______, _______,          _______, NG_SHFTL, _______,_______,NG_SHFTR, _______,          _______, _______, _______, _______ \
   ),
 
   /* Lower
@@ -74,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |                    |      | Left | Down |  Up  | Right|      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      | Shift| Prev |      |      |      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      | Shift| Prev |      | Paste|      |      |      |      |      |      |      |
    * |-------------+------+------+------+------+   +  +------+------+------+------+------+------+------+------+-------------|
    * |      |      |      |      ||||||||      | Space|      ||||||||      |      |      ||||||||      |      |      |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -83,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  _______,                       _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
     _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, KC_F12,  \
     _______, _______, _______, _______, _______, _______,_______,                       _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-    _______, _______, _______, _______, _______, _______,LCTL(LSFT(KC_TAB)),            _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,LCTL(LSFT(KC_TAB)),         LCTL(KC_V), _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______,          _______,LSFT(KC_SPC),_______,  _______,_______, _______,          _______, _______, _______, _______  \
   ),
 
@@ -95,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |                    |      | Left | Down |  Up  | Right|      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      | Shift| Prev |      |      |      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      | Shift| Prev |      | Paste|      |      |      |      |      |      |      |
    * |-------------+------+------+------+------+   +  +------+------+------+------+------+------+------+------+-------------|
    * |      |      |      |      ||||||||      | Space|      ||||||||      |      |      ||||||||      |      |      |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -104,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  _______,                       _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
     _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, KC_F12,  \
     _______, _______, _______, _______, _______, _______,_______,                       _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-    _______, _______, _______, _______, _______, _______,LCTL(LSFT(KC_TAB)),            _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,LCTL(LSFT(KC_TAB)),         LCTL(KC_V), _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______,          _______,LSFT(KC_SPC),_______,  _______,_______, _______,          _______, _______, _______, _______  \
   ),
 
@@ -122,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_ADJUST] = LAYOUT(
-    RESET,QWERTY_WIN,QWERTY_MAC,_______,_______, _______,_______,                      _______, _______, _______, _______, _______, _______, _______, \
+    RESET,QWERTY_WIN,QWERTY_MAC,_______,_______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______, \
     VRSN,    _______, _______, KC_MS_U, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,_______,                       _______, _______, _______, _______, _______, _______, KC_MPLY, \
     _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, KC_MPRV, KC_MNXT, _______, \
@@ -134,9 +162,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
 #endif
 
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
+void matrix_init_user(void) {
+  set_nicola(_NICOLA);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -147,14 +174,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         set_single_persistent_default_layer(_QWERTY_WIN);
       }
       return false;
-      break;
     case QWERTY_MAC:
       if (record->event.pressed) {
         print("mode just switched to mac and this is a huge string\n");
         set_single_persistent_default_layer(_QWERTY_MAC);
       }
       return false;
-      break;
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
@@ -164,7 +189,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-      break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
@@ -174,7 +198,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-      break;
     case ADJUST:
       if (record->event.pressed) {
         layer_on(_ADJUST);
@@ -182,7 +205,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_ADJUST);
       }
       return false;
-      break;
     case VRSN:
       if (record->event.pressed) {
         SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
@@ -192,7 +214,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         key_timer = timer_read(); // if the key is being pressed, we start the timer.
       } else { // this means the key was just released, so we can figure out how long it was pressed for (tap or "held down").
-        if (timer_elapsed(key_timer) < 200) {
+        if (timer_elapsed(key_timer) < TAPPING_TERM) {
           SEND_STRING(SS_LGUI("c"));
         } else {
           SEND_STRING(SS_LGUI("x"));
@@ -201,15 +223,45 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
     case C_CX:
       if (record->event.pressed) {
-        key_timer = timer_read(); // if the key is being pressed, we start the timer.
-      } else { // this means the key was just released, so we can figure out how long it was pressed for (tap or "held down").
-        if (timer_elapsed(key_timer) < 200) {
+        key_timer = timer_read();
+      } else {
+        if (timer_elapsed(key_timer) < TAPPING_TERM) {
           SEND_STRING(SS_LCTRL("c"));
         } else {
           SEND_STRING(SS_LCTRL("x"));
         }
       }
       return false;
+    case EISU:
+      if (record->event.pressed) {
+        key_timer = timer_read();
+        register_code(KC_LALT);
+      } else {
+        if (timer_elapsed(key_timer) < TAPPING_TERM) {
+          nicola_off();
+        }
+        unregister_code(KC_LALT);
+      }
+      return false;
+    case KANA:
+      if (record->event.pressed) {
+        key_timer = timer_read();
+        register_code(KC_RALT);
+      } else {
+        if (timer_elapsed(key_timer) < TAPPING_TERM) {
+          nicola_on();
+        }
+        unregister_code(KC_RALT);
+      }
+      return false;
   }
+
+  bool processed = true;
+  if (nicola_state()) {
+    nicola_mode(keycode, record);
+    processed = process_nicola(keycode, record);
+  }
+  if (processed == false) return false;
+
   return true;
 }
