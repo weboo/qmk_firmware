46668c602d9a99b57c040fd6fda221c05ec936f3
weboo <weboo@users.noreply.github.com>
2021-01-09 23:48:12 +0900

    modified to use tap dance for copy/cut

--- keyboards/ergodash/rev1/keymaps/weboo/keymap.c
+++ keyboards/ergodash/rev1/keymaps/weboo/keymap.c
@@ -28,6 +28,15 @@ enum custom_keycodes {
   EISU,
 };
 
+// Tap Dance definitions
+enum {
+  TD_COPY_CUT,
+};
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_COPY_CUT] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_C), LCTL(KC_X)),
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty for Windows
@@ -47,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                          KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                         KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                         KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            LCTL(KC_TAB), C_CX,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            LCTL(KC_TAB), TD(TD_COPY_CUT),    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
     KC_PGUP,KC_PGDOWN,KC_LGUI, EISU,            LOWER,    KC_SPC, LGUI(KC_S),   KC_ENTER,  KC_RCTL, RAISE,             KANA,    KC_BSPC, KC_HOME, KC_END   \
   ),
 
